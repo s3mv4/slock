d5f6ad2d1da29d07a6f4a6fe9a600503f2da47ad
s3m <s3m@archlinux.org>
2022-12-22 13:57:00 +0100

    changed colors

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#000000",     /* after initialization */
+	[INPUT] =  "#458588",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
